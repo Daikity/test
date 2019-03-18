f6f8d4da8af42c2f139e1a9d4ba503b8a11c01f1
Igor Pivnev <roodyept@gmail.com>
2019-03-18 10:04:52 +0300

    add search funtion

--- openui5-basic-template-app/webapp/controller/Home.controller.js
+++ openui5-basic-template-app/webapp/controller/Home.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"webapp/controller/BaseController",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
 	"../model/formatter"
 	
-], function(BaseController, formatter, ) {
+], function(BaseController, Filter, FilterOperator, formatter) {
 	"use strict";
 
 	return BaseController.extend("webapp.controller.App", {
@@ -10,6 +12,56 @@ sap.ui.define([
 		formatter: formatter,
 
 		onInit: function () {
+			var oRouter = this.getRouter();
+			this._oTable = this.byId("peopleList");
+			this._sSearchQuery = null;
+			this._oRouterArgs = null;
+
+			oRouter.getRoute("home").attachMatched(this._onRouteMatched, this);
+
+			
+		},
+
+		_onRouteMatched : function (oEvent) {
+			
+			this._oRouterArgs = oEvent.getParameter("arguments");
+			this._oRouterArgs.query = this._oRouterArgs["?query"] || {};
+
+			if (this._oRouterArgs.query) {
+				
+				this._applySearchFilter(this._oRouterArgs.query.search);
+			}
+			
+		},
+		_applySearchFilter : function (sSearchQuery) {
+			var aFilters, oFilter, oBinding;
+
+			// first check if we already have this search value
+			if (this._sSearchQuery === sSearchQuery) {
+				return;
+			}
+			this._sSearchQuery = sSearchQuery;
+			this.byId("searchField").setValue(sSearchQuery);
+
+			// add filters for search
+			aFilters = [];
+			if (sSearchQuery && sSearchQuery.length > 0) {
+				aFilters.push(new Filter("FirstName", FilterOperator.Contains, sSearchQuery));
+				aFilters.push(new Filter("LastName", FilterOperator.Contains, sSearchQuery));
+				oFilter = new Filter({ filters: aFilters, and: false });  // OR filter
+			} else {
+				oFilter = null;
+			}
+
+			// update list binding
+			oBinding = this._oTable.getBinding("items");
+			oBinding.filter(oFilter, "Application");
+		},
+		onSearchEmployeesTable : function (oEvent) {
+			var oRouter = this.getRouter();
+			
+			this._oRouterArgs.query.search = oEvent.getSource().getValue();
+			oRouter.navTo("home",this._oRouterArgs, true);
 		},
 
 		onNext : function (oEvent) {
